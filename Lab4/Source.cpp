1de71b391a09bba43af1e98fa597d3396ea34fb9
iliuha1907 <60312075+iliuha1907@users.noreply.github.com>
2020-03-20 17:05:46 +0300

    Update Source.cpp

--- Lab4/Source.cpp
+++ Lab4/Source.cpp
@@ -7,19 +7,19 @@ using namespace std;
 CRITICAL_SECTION cs;
 
 /*
-*Это массивы событий. Первый - для main от каждого треда, который по своему номеру ставит событие.
-*Второй - для сообщений каждому треду от main. Индекс - это также номер треда.
+* These are arrays of events. The first is for main from each thread that sets an event by its number.
+* Second - for messages to each thread from main. An index is also a thread number.
 */
 HANDLE *hOutEvent, *hMainEvent;
 
-volatile int* arr;//это основной массив
+volatile int* arr;//this is base array
 int sizeArr;
-volatile bool* isOver;//через этот массив каждый тред смотрит, не отправлен ли запрос на завершение
+volatile bool* isOver;//through this array, each thread detects if a completion request has been sent
 
-/*Функция, которую выполняют треды*/
+/*The function that threads perform*/
 void thread(int num)
 {
-	vector<int> marked;//здесь хранятся индексы помеченных элементов
+	vector<int> marked;//indexes of marked items are stored here
 	srand(num);
 	while(true)
 	{
@@ -28,14 +28,14 @@ void thread(int num)
 		cout << "Hello from " << num + 1 << ", Number = " << index << endl;
 		LeaveCriticalSection(&cs);
 		
-		if (arr[index] == 0)//если не 0, то точно на завершение
+		if (arr[index] == 0)// if not 0, then exactly stop
 		{
-			EnterCriticalSection(&cs);//здесь несколько тредов теоритически могли пройти проверку, поэтому нужна еще одна
+			EnterCriticalSection(&cs);//here several threads could theoretically pass the test, so we need one more
 			if (arr[index] == 0)
 			{
 				Sleep(5);
 				arr[index] = num + 1;
-				marked.push_back(index);//добавляем индекс
+				marked.push_back(index);//adds index
 				Sleep(5);
 			}
 			LeaveCriticalSection(&cs);
@@ -46,12 +46,12 @@ void thread(int num)
 			cout << "Thread #" << num + 1 << ", marked " << marked.size() << " elements, unable to mark at index " << index << endl;
 			LeaveCriticalSection(&cs);
 
-			SetEvent(hOutEvent[num]);//тред говорит main, что он остановился. По своему номеру выставляет событие
-			WaitForSingleObject(hMainEvent[num], INFINITE);//теперь по своему номеру ожидает ответ от main
+			SetEvent(hOutEvent[num]);//the thread tells main that it has stopped. It sets an event by its number
+			WaitForSingleObject(hMainEvent[num], INFINITE);/// now expects a response from main by its number
 
-			if (isOver[num])//если он был выбран для завершения
+			if (isOver[num])//if it was selected to stop
 			{
-				for (int i = 0; i < marked.size(); i++)//по всем своим помеченным индексам ставит 0
+				for (int i = 0; i < marked.size(); i++)//for all its marked indexes it puts 0
 				{
 					EnterCriticalSection(&cs);
 					arr[marked[i]] = 0;
@@ -63,23 +63,23 @@ void thread(int num)
 	}
 }
 
-/*Функция создает события, а также треды*/
+/*The function creates events as well as threads.*/
 void initThrEv(HANDLE* hThread, DWORD &IDThread, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		//создаем тред, который сразу запущен не будет
+		//creates a thread that will not be launched immediately
 		hThread[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread, (LPVOID)i, CREATE_SUSPENDED, &IDThread);
 		if (hThread[i] == NULL)
 		{
 			exit(GetLastError());
 		}
-		hOutEvent[i] = CreateEvent(NULL, TRUE, FALSE, NULL);//с ручным сбросом
+		hOutEvent[i] = CreateEvent(NULL, TRUE, FALSE, NULL);//with manual reset
 		if (hOutEvent[i] == NULL)
 		{
 			exit(GetLastError());
 		}
-		hMainEvent[i] = CreateEvent(NULL, FALSE, FALSE, NULL);//с авто сбросом
+		hMainEvent[i] = CreateEvent(NULL, FALSE, FALSE, NULL);//with auto reset
 		if (hMainEvent[i] == NULL)
 		{
 			exit(GetLastError());
@@ -87,7 +87,7 @@ void initThrEv(HANDLE* hThread, DWORD &IDThread, int n)
 	}
 }
 
-/*Функция для запуска каждого созданного треда*/
+/*Function to launch each created thread*/
 void startTHR(HANDLE* hThread, int n)
 {
 	for (int i = 0; i < n; i++)
@@ -96,12 +96,12 @@ void startTHR(HANDLE* hThread, int n)
 	}
 }
 
-/*функция для сброса событий*/
+/*function to reset events*/
 void resEvents(int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (!isOver[i])//нужно сбросить событие, если тред будет в состоянии его выставить (еще существует)
+		if (!isOver[i])//you need to reset the event if the thread will be able to set it (still exists)
 		{
 			ResetEvent(hOutEvent[i]);
 			SetEvent(hMainEvent[i]);
@@ -135,29 +135,29 @@ int main()
 	hOutEvent = new HANDLE[n];
 	hMainEvent = new HANDLE[n];
 	isOver = new bool[n] {false};
-    arr = new int[sizeArr] {0};
+        arr = new int[sizeArr] {0};
 
-	initThrEv(hThread,IDThread,n);//создает события, а также треды, которые не будут запущены сразу
+	initThrEv(hThread,IDThread,n);//creates events, as well as threads that will not be launched immediately
 	
-	startTHR(hThread, n);//вот теперь запуск
+	startTHR(hThread, n);//now launch
 
-	while (countLeft !=n)//пока не остановлены все
+	while (countLeft !=n)//until everyone is stopped
 	{
 		int number;
-		WaitForMultipleObjects(n, hOutEvent, TRUE, INFINITE);//ждет все события от всех тредов
+		WaitForMultipleObjects(n, hOutEvent, TRUE, INFINITE);//waits for all events from all threads
 
 		printArray(arr, sizeArr);
 		cout << "Enter number of THR to stop" << endl;
 		cin >> number;
 		isOver[number-1] = true;
 
-		SetEvent(hMainEvent[number-1]);//говорит конкретному треду продолжить работу
-		WaitForSingleObject(hThread[number-1], INFINITE);//ждет завершение этого треда
+		SetEvent(hMainEvent[number-1]);//tells a specific thread to continue the work
+		WaitForSingleObject(hThread[number-1], INFINITE);//waiting for the completion of this thread
 
 		printArray(arr, sizeArr);
-		resEvents(n);//сбрасывает события, но только для еще существующих тредов
+		resEvents(n);//resets events, but only for existing threads
 
-		countLeft++;//счетчик остановленных тредов
+		countLeft++;//stopped threads counter
 	}
 	
 	DeleteCriticalSection(&cs);
